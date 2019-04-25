c9dc5f9530305645cf75a6ffacaf158d33c945d1
Rajan Kumar <rajankrhp@gmail.com>
2019-04-25 22:47:39 +0530

    second commit

--- project.cpp
+++ project.cpp
@@ -61,3 +61,76 @@ void station_load_train(struct station *station, int count)
   pthread_mutex_unlock(&(station->lock));
 }
 
+void station_wait_for_train(struct station *station)
+{
+  pthread_mutex_lock(&(station->lock));
+
+  station->waiting_passengers++;
+  pthread_cond_wait(&(station->train_arrived), &(station->lock));
+  station->waiting_passengers--; /* passenger got the seat */
+  station->seated_passengers++;
+
+  pthread_mutex_unlock(&(station->lock));
+
+  pthread_cond_signal(&(station->passengers_seated));
+}
+
+void station_on_board(struct station *station)
+{
+  pthread_mutex_lock(&(station->lock));
+
+  station->seated_passengers--;
+
+  pthread_mutex_unlock(&(station->lock));
+
+  if (station->seated_passengers == 0)
+  	pthread_cond_broadcast(&(station->train_full));
+}
+
+
+
+void* passenger_thread(void *arg)
+{
+	struct station *station = (struct station*)arg;
+	station_wait_for_train(station);
+	__sync_add_and_fetch(&threads_completed, 1);
+	return NULL;
+}
+
+
+void* train_thread(void *args)
+{
+	struct load_train_args *l = (struct load_train_args*)args;
+	station_load_train(l->station, l->free_seats);
+	load_train_returned = 1;
+	return NULL;
+}
+
+int main()
+{
+     int total_passengers; 
+    printf("************************************************************************************************************************\n");
+    printf("************************************************************************************************************************\n");
+    printf("||||||||||||||||||||||||||||||||||||||  /**INDIAN RAIL WELCOMES YOU **/|||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf("|||||||||||||||||||||||||||||||||||||| /**TRAVEL SAFE TRAVEL WITH COMFORT**/ |||||||||||||||||||||||||||||||||||||||||||\n");
+    printf("************************************************************************************************************************\n");
+    printf("************************************************************************************************************************\n");
+    printf("\nEnter Total No Of Passengers Waiting At The Station: ");
+	scanf("%d",&total_passengers);
+//	printf("\n  Total No of passengers are : %d\n",total_passengers);
+	struct station station;
+	station_init(&station);
+	int passengers_left = total_passengers;
+	station_load_train(&station, 0);
+	station_load_train(&station, 10);
+	// Create a bunch of 'passengers', each in their own thread.
+	int i;
+	for (i = 0; i < total_passengers; i++) {
+		pthread_t a;
+		int cr = pthread_create(&a, NULL, passenger_thread, &station);
+		if (cr != 0) {
+			perror("pthread_create");
+			exit(1);
+		}
+	}
+
